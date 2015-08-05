575deed9f62f005843c152cf0a529dbd3cb852fa
sujayraaj <sujayraaj@gmail.com>
2015-08-06 03:12:41 +0530

    In memory filesystem for monkey on rtems. Has config files and server data. Only for testing purposes.

---
+++ mk_bin/rtems/rtems_filesystem.h
@@ -0,0 +1,15 @@
+/*
+ *  Extern declarations for C structure representing binary file Filesys
+ *
+ *  WARNING: Automatically generated -- do not edit!
+ */
+
+#ifndef __Filesystem_h
+#define __Filesystem_h
+
+#include <sys/types.h>
+
+extern const unsigned char Filesys[];
+extern const size_t Filesys_size;
+
+#endif
